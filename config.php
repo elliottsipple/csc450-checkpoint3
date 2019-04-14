14caaa0b6e8e1eec172a605a8518d09fb32c1ce6
Elliott Sipple <elliott@elliotts-mbp.hh.nku.edu>
2019-04-13 20:45:13 -0400

    cleaned up config file

--- config.php
+++ config.php
@@ -1,28 +1,23 @@
 <?php
 
 // Connect to the Oracle database
-$tns = "
-(DESCRIPTION =
+$tns = "(DESCRIPTION =
   (ADDRESS = (PROTOCOL = TCP)(HOST = CITDB.NKU.EDU)(PORT = 1521))
-  (CONNECT_DATA =
-    (SERVER = DEDICATED)
-    (SERVICE_NAME = csc450.citdb.nku.edu)
-  )
-)";
+  (CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = csc450.citdb.nku.edu)))";
 $db_username = "SIPPLEE1";
 $db_password = "csc684";
 
 try {
-  $conn = new PDO("oci:dbname=".$tns,$db_username,$db_password);
+    $conn = new PDO("oci:dbname=".$tns,$db_username,$db_password);
 } catch(PDOException $e) {
-  echo ($e->getMessage());
+    echo ($e->getMessage());
 }
 
 // autoload classes
 function my_autoloader($class) {
-		include 'classes/class.' . $class . '.php';
-	}
-	spl_autoload_register('my_autoloader');
+	include 'classes/class.' . $class . '.php';
+}
+spl_autoload_register('my_autoloader');
 
 // Start the session
 session_start();
@@ -31,11 +26,11 @@ $current_url = basename($_SERVER['REQUEST_URI']);
 
 // if username is not set in the session and current URL not login.php redirect to login page
 if (!isset($_SESSION["userID"]) && $current_url != 'login.php') {
-  header("Location: login.php");
+    header("Location: login.php");
 }
 
 // Else if session key username is set get $user from the database
 elseif (isset($_SESSION["userID"])) {
 	// new User
-  $user = new User($_SESSION["userID"], $conn);
+    $user = new User($_SESSION["userID"], $conn);
 }
